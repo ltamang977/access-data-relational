f75efc6df71ec87486cf45743466c727b9d36045
ltamang977 <ltamang977@gmail.com>
2020-07-13 22:34:24 -0700

    First commit

---
+++ src/main/java/org/lv/accessdatarelational/AccessDataRelationalApplication.java
@@ -0,0 +1,13 @@
+package org.lv.accessdatarelational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessDataRelationalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessDataRelationalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lv/accessdatarelational/AccessDataRelationalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lv.accessdatarelational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessDataRelationalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
